04cf792dccf7c2384fd12362196791bb99f8cf91
Chris Mitsopoulos <chrismits@Chriss-MacBook-Pro.local>
2019-09-24 21:10:58 -0400

    Working extension

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,105 +1,42 @@
-
-//alert("Welcome to Netflix");
-// var xpos, ypos;
-
-// document.onmouseover = function(e) {
-//     e = e || window.event;
-//     xpos = e.clientX;
-//     ypos = e.clientY;
-//     var elem = elementAtMouse();
-//     var title = elem.getAttribute("aria-label");
-
-
-// };
-
-
+//Content script that only runs when in netflix domain. Periodically checks for new titles every second
 console.log("Running")
-//chrome.tabs.goBack()
 
-var prevURL = '';
-var prev_title = '';
-setInterval(function() {
-   // if (document.location.href != prevURL) {
-        // do something
-       // prevURL = document.location.href;
-
-        $("body").find("span.duration").each( function() {
-
-            var tmp = $(this).next()
-            var copy = this
-            if (!tmp.hasClass("myrating")) {
-                var title = getTitle(copy, prev_title)
-                if (title != "N/A") {
-                    prev_title = title
-                    chrome.runtime.sendMessage({movie: title}, function(response) {
-                        // $(this).after("<span class=\"myrating\">" + response.imdbRating + "TEST</span>")
-                        console.log("Sending message")
-                        console.log(response)
-
-                        if (response.imdb && response.imdb != "N/A") {
-                           // console.log(response.imdb)
-                            var html = "<span class=\"my__rating\"> " + response.imdb + "</span>"
-                            console.log(html)
-                            if (!$(copy).next()) {
-                                $(copy).after(html).next().css({
-                                    "border": "2px solid #FFFFFF",
-                                    "font-size": "14px",
-                                    "letter-spacing": "2px",
-                                    "word-spacing": "2px",
-                                    "color": "#FFFFFF",
-                                    "font-weight": "700",
-                                    "text-decoration": "none",
-                                    "font-style": "normal",
-                                    "font-variant": "normal",
-                                    "text-transform": "uppercase"
-                                })
-                            }
-                            return
-                        }
-                    });
-                }
-            }
-        });
-
-   // }
-
-}, 1000);
-
-
-
-
-
-function getTitle(obj, previous) {
+function getTitle(obj) {
     var ancestor = $(obj).closest("div.jawBone")
     var h1 = $(ancestor).children("h1").find("img").attr("alt")
     var h3 = $(ancestor).children("h3").find("img").attr("alt")
     var movie_title = "N/A"
 
-    if (h1) {
+    if (h1)
         return "N/A"
-    }
-    else if (previous == h3) {
-        return "N/A"
-    }
-    else {
+    else
         return h3
-    } 
 }
 
-
-
-
-
-// document.onmousemove = function() {
-//     console.log("click")
-    
-//     $("body").find("span.duration").each(function(){
-//         var tmp = $(this).next()
-//         if (!tmp.hasClass("myrating")) {
-//             getRating(this)
-//             // console.log(tmp)   
-//             // if (rating != "N/A")
-//             //     $(this).after("<span class=\"myrating\">" + rating + "TEST</span>")
-//         }
-//     });
-// };
+var prev_title = '';
+var prev_rate = ''
+setInterval(function() {
+        $("body").find("span.duration").each( function() {
+            var tmp = $(this).next()
+            var copy = this
+            if (!tmp.hasClass("myrating")) {
+                var title = getTitle(copy)
+                if (title != "N/A" && title != prev_title) {
+                    prev_title = title
+                    chrome.runtime.sendMessage({movie: title}, function(response) {
+                        if (response.imdb && response.imdb != "N/A" && response.imdb != prev_rate) {
+                            prev_rate = response.imdb
+                            var html = "<span class=\"my__rating\">   IMDb: " + response.imdb + "  </span>"
+                                $(copy).after(html).next().css({
+                                    "font-size": "px",
+                                    "border": "2px solid #FFFFFF",
+                                    "border-radius": "20px",
+                                    "padding": "5px 5px 5px 5px"
+                                })
+                        }
+                        return true
+                    });
+                }
+            }
+        });
+}, 1000);
--- scripts/movieDataBGR.js
+++ scripts/movieDataBGR.js
@@ -1,27 +1,9 @@
-//send to content script]
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//     chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
-//       console.log(response.farewell);
-//     });
-//   });
-
-// function getIMDBrating(movie) {
-//   var rating;
-//   $.getJSON('http://www.omdbapi.com/?apikey=54386718&t=' + encodeURI(movie)).then(function(response) {
-//     if (response != null) {
-//       if (response.imdbRating) {
-//          rating = response.imdbRating;
-//       }
-//     }
-//   });
-
-//   return rating;
-// }
-
-//receive from content script
+//Listen for movie from content Script
+var requested = ''
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-      if (request.movie != "" && request.movie != null) {
+      if (request.movie != "" && request.movie != null && requested != request.movie) {
+        requested = request.movie
         console.log("HELLO");
         //get IMDB rating
         $.getJSON('http://www.omdbapi.com/?apikey=54386718&t=' + encodeURI(request.movie)).then(function(response) {
@@ -32,7 +14,4 @@ chrome.runtime.onMessage.addListener(
       }
 
     return true;
-    });
-
-    // $.getJSON('https://www.omdbapi.com/?apikey=54386718&t=' + encodeURI(request.movie)).then(function(response) {
-    // });
+  });
